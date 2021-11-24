5bd7ea72bd006ae4330bbb1e3bb2ee5ec2da3984
ofoe-fiergbor <ofoefiergbor16@gmail.com>
2021-11-24 16:56:43 +0000

    initial commit

---
+++ src/test/java/com/group19/marketdataservice/MarketDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group19.marketdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
